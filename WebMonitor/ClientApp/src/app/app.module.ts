8cc7122f608ec26fef7af702ff5975a9e2c89db6
LukaStrbad <s.luka.1308@gmail.com>
2023-04-16 22:04:28 +0200

    Remove unnecessary components

--- WebMonitor/ClientApp/src/app/app.module.ts
+++ WebMonitor/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
@@ -17,18 +15,14 @@ import { MatCardModule } from "@angular/material/card";
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    HomeComponent
   ],
     imports: [
         BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
         HttpClientModule,
         FormsModule,
         RouterModule.forRoot([
-            {path: '', component: HomeComponent, pathMatch: 'full'},
-            {path: 'counter', component: CounterComponent},
-            {path: 'fetch-data', component: FetchDataComponent},
+            {path: '', component: HomeComponent, pathMatch: 'full'}
         ]),
         BrowserAnimationsModule,
         MatButtonModule,
