5c687d194b4664e4b848a7f9f565350c847f7750
Luka Strbad <s.luka.1308@gmail.com>
2023-08-14 18:21:27 +0200

    Add ability to build multiple versions of the terminal plugin

--- TerminalPlugin/build.sh
+++ TerminalPlugin/build.sh
@@ -8,10 +8,17 @@ npm install
 ncc build ./src/index.js -o ../build
 cd ..
 
-mkdir -p build/linux-x64
+# Get node version and trim leading 'v'
+nodeVersion=$(node --version)
+nodeVersion=${nodeVersion:1}
+
+linuxDir="linux-x64-node_$nodeVersion"
+
+mkdir -p build/$linuxDir
 
 dotnet publish TerminalPlugin.csproj --configuration Release -o build/plugin
 
-mv build/build build/linux-x64
-mv build/index.js build/linux-x64
-mv build/plugin/TerminalPlugin.dll build/linux-x64
+mv build/build build/$linuxDir
+mv build/index.js build/$linuxDir
+mv build/plugin/TerminalPlugin.dll build/$linuxDir
+echo $nodeVersion > build/$linuxDir/node-version.txt
